063ffaacce01081a75a379158bc929691099db07
ndhu410421304 <33059129+ndhu410421304@users.noreply.github.com>
2019-11-29 00:19:09 +0800

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -21,7 +21,7 @@ boxcount = 0
 
 trainfile = open("train.txt","w")
 valfile = open("val.txt","w")
-Qfile = trainfile # current opened file
+cur_file = trainfile # current opened file
 process_count = 0 # count the image had beend process
 
 # generate path to h5 file
@@ -34,26 +34,27 @@ def get_bbox(index, hdf5_data):
     # 80% for train
     global trainfile
     global valfile
-    global Qfile
+    global cur_file
     global process_count
     if(process_count < 26756): # complete extract from training dataset
-        Qfile = trainfile
-        Qfile.write("%s data/train/" % process_count)
+        cur_file = trainfile
+        cur_file.write("%s data/train/" % process_count)
     else:
-        Qfile = valfile
-        Qfile.write("%s data/val/" % str(process_count - 26756))
+        cur_file = valfile
+        cur_file.write("%s data/val/" % str(process_count - 26756))
     
     global boxcount # annouce as global
     global cur_name # annouce as global
 
-    Qfile.write(cur_name + " ")
+    cur_file.write(cur_name + " ")
 
     attrs = {}
     item = hdf5_data['digitStruct']['bbox'][index].item()
     keynum = 0
-    tmpheight, tmpwidth = cv2.imread(os.path.join(train_folder,cur_name)).shape[:2]
+    # load image to get height, width
+    tmp_height, tmp_width = cv2.imread(os.path.join(train_folder,cur_name)).shape[:2]
 
-    Qfile.write(str(tmpwidth) + " " + str(tmpheight) + " ")
+    cur_file.write(str(tmp_width) + " " + str(tmp_height) + " ")
 
     '''
     Notice not to let bounding box range
@@ -82,37 +83,37 @@ def get_bbox(index, hdf5_data):
                 ymin.append(max(values[i], 1))
 
             elif keynum == 3:
-                xmax.append(min(values[i]+xmin[boxcount+i], tmpwidth - 1))
+                xmax.append(min(values[i]+xmin[boxcount+i], tmp_width - 1))
                 print(values[i]+xmin[boxcount + i])
 
             # For last keynum, we would construct the data format we are going to
             # use for the model
             elif keynum == 4:
-                ymax.append(min(ymin[boxcount + i] + values[i], tmpheight - 1))
+                ymax.append(min(ymin[boxcount + i] + values[i], tmp_height - 1))
                 # chnage 0 to 10: for yolo model
                 # add spacing for the model required file format
                 if(int(labels[boxcount + i]) == 10):
-                    Qfile.write(str("0" + " "))
+                    cur_file.write(str("0" + " "))
                 else:
-                    Qfile.write(str(int((labels[boxcount + i]))) + " ")
-                Qfile.write(str(int(xmin[boxcount + i])) + " ")
-                Qfile.write(str(int(ymin[boxcount + i])) + " ")
-                Qfile.write(str(int(xmax[boxcount + i])) + " ")
-                Qfile.write(str(int(ymax[boxcount + i])) + " ")
+                    cur_file.write(str(int((labels[boxcount + i]))) + " ")
+                cur_file.write(str(int(xmin[boxcount + i])) + " ")
+                cur_file.write(str(int(ymin[boxcount + i])) + " ")
+                cur_file.write(str(int(xmax[boxcount + i])) + " ")
+                cur_file.write(str(int(ymax[boxcount + i])) + " ")
                 # append current img name as well
                 img_name.append(cur_name)
                 print(cur_name)
                 if i == (len(values) - 1):
                     boxcount += len(values)
                     # read image size
-                    img_h, img_w = tmpheight, tmpwidth
+                    img_h, img_w = tmp_height, tmp_width
                     print(img_h, img_w)
                     for j in range(len(values)):
                         img_widths.append(img_w)
                         img_heights.append(img_h)
         keynum += 1
     process_count += 1
-    Qfile.write("\n")
+    cur_file.write("\n")
 
 def img_boundingbox_data_constructor(mat_file):
     f = h5py.File(mat_file,'r') 
